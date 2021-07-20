9bd5f5cbd943b9d6b66451024c681c1eea280d5d
MikeDudolevitch <mikedudolevitch@gmail.com>
2021-07-20 11:45:00 -0400

    sharing build for cohort mates

---
+++ frontend/source/image.js
@@ -0,0 +1,35 @@
+class Image {
+    constructor({name, link}) {
+        this.name = name
+        this.link = link
+        Image.all.push(this)
+    } 
+    static all = []
+
+    addToDom() {
+        const imageDiv = document.querySelector("#img-container")
+        imageDiv.innerHTML += this.render()
+    }
+    
+    render() {
+        return (`<h3> ${this.name} </h3>
+        <img src= "${this.link}" />`)
+    
+    }
+
+    // const getImage = () {
+    // fetch('http://localhost:3000/api/images')
+    // .then(resp => resp.json())
+    // .then(image => console.log(image))
+    // .catch(error => console.warn(error))
+    // }
+    
+
+
+    // renderImages() 
+    //     const imageDiv = document.querySelector("#img-container")
+    //     const pic = document.createElement('image')
+
+    // }
+
+}
---
+++ frontend/source/index.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', () => {
+    getImage()
+})
+
+function getImage(){
+    fetch('http://localhost:3000/api/images') 
+    .then(r => r.json())
+    .then(data => {
+        data.forEach(image => {
+            const i = new Image(image)
+            i.addToDom()
+        })
+    })
+    .catch(err => console.warn(err))
+}
+
+function addToDom() {
+    const imageDiv = document.querySelector("#img-container")
+    imageDiv.innerHTML += this.render()
+}
+
+function render() {
+    return (`<h3> Generic Name </h3>
+    <img src= "${Image.link}" />`)
+
+}
